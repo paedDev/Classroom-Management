5c71ed4b92122ee19188ef8430df3aa13afe877d
Jan Noel S. Paed <Jannoelpaed27@gmail.com>
2026-05-03 18:07:07 +0800

    implement GET all subjects route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import subjectRouter from "./routes/subjects.js";
+import cors from "cors";
 dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 5000;
+const corsOptions = {
+    origin : process.env.FRONTEND_URL || "http://localhost:3000",
+    methods:['GET','POST','PUT','DELETE'],
+    credentials:true
+}
+app.use(cors())
+
 
 app.get("/" ,(req,res) => {
  res.send("Hello, Welcome to the classroom API")
 })
-
+app.use('/api/subjects',subjectRouter)
 app.listen (PORT,() => {
     console.log(`Server is running on port ${PORT}`);
     
